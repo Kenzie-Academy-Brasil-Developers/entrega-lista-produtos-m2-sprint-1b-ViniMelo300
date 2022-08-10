b0fed46be9bfd48b63382eac788a5d6cbea4dc38
vinicius <gamesleonar@gmail.com>
2022-08-10 17:01:34 -0300

    Entrega feita parte 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,143 @@
 const tagUl = document.querySelector("ul")
+const carrinhoUl = document.querySelector(".ulCarrinho")
+const produtosCarrinho = []
+let todosValores = 0
 
-const criarCard = (arr) => {
-    const tagLi = document.createElement("li")
-    const img   = document.createElement("img")
-    const h3    = document.createElement("h3")
-    const span  = document.createElement("span")
-    const p     = document.createElement("p")
+function criarCard(produtoObjeto){
+    const produtosLi    = document.createElement("li")
+    const imagemProduto = document.createElement("img")
+    const nomeProduto   = document.createElement("h3")
+    const secao         = document.createElement("span")
+    const nutrientesOl  = document.createElement("ol")
+    const div           = document.createElement("div")
+    const precoP        = document.createElement("p")
+    const botao         = document.createElement("button")
 
-    img.src        = arr.img
-    img.alt        = arr.nome
-    h3.innerText   = arr.nome
-    span.innerText = arr.categoria
-    p.innerText    = arr.preco
+    botao.addEventListener("click", () => {
+        produtosCarrinho.push(produtoObjeto)
+        renderizarCardCarrinho(produtosCarrinho)
+        todosValores += produtoObjeto.preco 
+        QuantidadeCarrinho(todosValores)
+        valorCarrinho(todosValores)
+    })
+
+    imagemProduto.src = produtoObjeto.img
+    imagemProduto.alt = produtoObjeto.nome
     
-    tagLi.append(img, h3, span, p)
-    return tagLi
+    let idProduto = produtoObjeto.id
+    botao.setAttribute("id", idProduto)
+
+    nomeProduto.innerText = produtoObjeto.nome
+
+    secao.innerText = produtoObjeto.secao
+
+    let precoFormatado = produtoObjeto.preco.toFixed(2).toString().replace(".", ",")
+    precoP.innerText = precoFormatado
+    botao.innerText  = "Comprar"
+
+    const arrayComponentes = produtoObjeto.componentes
+    arrayComponentes.forEach((componente) => {
+        const listaNutrientes = document.createElement("li")
+        listaNutrientes.innerText = componente
+        nutrientesOl.appendChild(listaNutrientes)
+    })
+
+    div.append(precoP, botao)
+    produtosLi.append(imagemProduto, nomeProduto, secao, nutrientesOl, div)
+
+    return produtosLi
 }
+
+function criarCardCarrinho(produtoCarrinho){
+    
+    const carrinhoLi = document.createElement("li")
+    const produtoImgCarrinho = document.createElement("img")
+    const nomeProdutoCarrinho = document.createElement("h3")
+    const secaoProdutoCarrinho = document.createElement("p")
+    const precoProdutoCarrinho = document.createElement("span")
+    const botaoRemover = document.createElement("img")
+
+    produtoImgCarrinho.src = produtoCarrinho.img
+    produtoImgCarrinho.alt = produtoCarrinho.nome
+
+    nomeProdutoCarrinho.innerText = produtoCarrinho.nome
+    
+    secaoProdutoCarrinho.innerText = produtoCarrinho.secao
+
+    let precoFormatado = produtoCarrinho.preco.toFixed(2).toString().replace(".", ",")
+    precoProdutoCarrinho.innerText = precoFormatado
+
+    botaoRemover.src = "src/img/trash.png"
+    botaoRemover.alt = "lixeira remover"
+    botaoRemover.setAttribute("class", "removerCarrinho")
+    botaoRemover.addEventListener("click", () => {
+        const index = produtosCarrinho.indexOf(produtoCarrinho)
+        produtosCarrinho.splice(index, 1)
+        renderizarCardCarrinho(produtosCarrinho)
+        todosValores -= produtoCarrinho.preco
+        QuantidadeCarrinho(todosValores)
+        valorCarrinho(todosValores)
+    })
+    botaoRemover.innerText = "Remover"
+
+    carrinhoLi.append(produtoImgCarrinho, nomeProdutoCarrinho, secaoProdutoCarrinho, precoProdutoCarrinho, botaoRemover)
+
+    return carrinhoLi    
+}
+
+function QuantidadeCarrinho(parametro){
+    const quantidadeCarrinhoDiv = document.querySelector(".quantidade-produtosCarrinho")
+    quantidadeCarrinhoDiv.innerHTML = ""
+
+    if(produtosCarrinho.length >= 1){
+        const quantidade = document.createElement("h3")
+        let quantidadeProduto = document.createElement("p")
+
+        quantidade.innerText = "Quantidade:"
+        quantidadeProduto.innerText = produtosCarrinho.length
+
+        quantidadeCarrinhoDiv.append(quantidade, quantidadeProduto)
+    }
+}
+
+function valorCarrinho(resultado){
+    const valorTotalDiv = document.querySelector(".valorTotal-produtos")
+    valorTotalDiv.innerHTML = ""
+
+    if(produtosCarrinho.length >= 1){
+        const total = document.createElement("h3")
+        let valorTotal = document.createElement("p")
+
+        total.innerText = "Total:"
+        let precoFormatado = resultado.toFixed(2).toString().replace(".", ",")
+        valorTotal.innerText = precoFormatado
+
+        valorTotalDiv.append(total, valorTotal)
+    }
+}
+
 
 const renderizarCard = (arr) => {
 
-    arr.forEach((elemento) => {
-        const cardElemento = criarCard(elemento)
+    arr.forEach((produtoVitrine) => {
+        const cardElemento = criarCard(produtoVitrine)
         tagUl.appendChild(cardElemento)
     })
 }
 renderizarCard(produtos)
 
+const renderizarCardCarrinho = (arr) => {
+    carrinhoUl.innerHTML = ""
+    arr.forEach((produtosCarrinho) => {
+        const cardCarrinho = criarCardCarrinho(produtosCarrinho)
+        carrinhoUl.appendChild(cardCarrinho)
+    })
+}
+
+
+//FUNÇÕES DE FILTRAGEM PARA OS SETORES E BUSCAS
 function filtrarTodos(secao){
     const produtosFiltrados = produtos.filter(product => product.secao === secao)
-    somarProdutos(produtosFiltrados)
     return renderizarCard(produtosFiltrados)
 }
 
@@ -48,7 +157,7 @@ btnPanificadora.addEventListener("click", filtrarPanificadora)
 
 function filtrarLaticinios(){
     tagUl.innerHTML = "";
-    filtrarTodos('Laticínio')
+    filtrarTodos('laticinios')
 }
 const btnLaticinios = document.getElementsByClassName("laticinios")[0]
 btnLaticinios.addEventListener("click", filtrarLaticinios)
@@ -56,28 +165,10 @@ btnLaticinios.addEventListener("click", filtrarLaticinios)
 function todosProdutos(){
     tagUl.innerHTML = "";
     renderizarCard(produtos)
-    somarProdutos(produtos)
+    // somarProdutos(produtos)
 }
 const btnTodosProdutos = document.getElementsByClassName("todos")[0]
 btnTodosProdutos.addEventListener("click", todosProdutos)
-
-function somarProdutos(produtos){
-    const precoProdutos = document.getElementsByClassName("precoDinamico")[0]
-    const valor = produtos.reduce((acumulador, currentValue) => acumulador + currentValue.preco,0)
-
-    precoProdutos.innerText = `R$${valor.toFixed(2).toString().replace(".", ",")}`
-}
-somarProdutos(produtos)
-
-let barraPesquisa = document.querySelector(".campoBuscaPorNome")
-let botaoPesquisa = document.querySelector(".containerBuscaPorNome button")
-
-botaoPesquisa.addEventListener("click", function(){
-    let valorPesquisa = barraPesquisa.value.toLowerCase().trim()
-    let btnResultado = barraBuscar(valorPesquisa)
-    somarProdutos(btnResultado)
-    renderizarCard(btnResultado)
-})
 
 function barraBuscar(pesquisa){
     let resultadoPesquisa = []
@@ -85,10 +176,29 @@ function barraBuscar(pesquisa){
     
     for(let i = 0; i < produtos.length; i++){
         
-        let nomesProdutos = produtos[i].nome.toLowerCase().trim()
+        let categoriasProdutos = produtos[i].categoria.toLowerCase().trim()
+        let secaoProdutos      = produtos[i].secao.toLowerCase().trim()
+        let nomesProdutos      = produtos[i].nome.toLowerCase().trim()
         if(nomesProdutos.includes(pesquisa)){
+            resultadoPesquisa.push(produtos[i])
+        }
+        else if(secaoProdutos.includes(pesquisa)){
+            resultadoPesquisa.push(produtos[i])
+        }
+        else if(categoriasProdutos.includes(pesquisa)){
             resultadoPesquisa.push(produtos[i])
         }
     }
     return resultadoPesquisa
 }
+
+
+let barraPesquisa = document.querySelector(".campoBuscaPorNome")
+let botaoPesquisa = document.querySelector(".containerBuscaPorNome button")
+
+botaoPesquisa.addEventListener("click", function(){
+    let valorPesquisa = barraPesquisa.value.toLowerCase().trim()
+    let btnResultado = barraBuscar(valorPesquisa)
+    renderizarCard(btnResultado)
+})
+
